2fa21719729e175a53f1b54a3f6b43813048b261
navinvaghela <navin7771338@gmail.com>
2025-07-08 17:26:18 +0530

    comparision

---
+++ 04_conparison.js
@@ -0,0 +1,31 @@
+// console.log(2 == 2)
+// console.log(2 > 2)
+// console.log(2 < 2)
+// console.log(2 <= 2)
+// console.log(2 >= 2)
+// console.log(2 != 2)
+
+
+// console.log("2" > 1) // true - javascript automatic convert string to number
+// console.log("02" > 1) // true - javascript automatic convert string to number
+
+
+console.log("null == 0",null == 0)
+console.log("null > 0",null > 0)
+console.log("null >= 0",null >= 0)
+console.log("null < 0",null < 0)
+console.log("null <= 0",null <= 0)
+console.log("null != 0",null != 0)
+
+
+console.log("undefined == 0",undefined == 0)
+console.log("undefined > 0",undefined > 0)
+console.log("undefined >= 0",undefined >= 0)
+console.log("undefined < 0",undefined < 0)
+console.log("undefined <= 0",undefined <= 0)
+console.log("undefined != 0",undefined != 0)
+
+
+console.log("2" === 2) 
+
+console.log(2 === 2) 
