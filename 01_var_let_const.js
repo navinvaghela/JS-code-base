2d84d8ef58a2995f0d9a3195e3736712533eee46
navinvaghela <navin7771338@gmail.com>
2025-07-08 15:39:45 +0530

    datatypes

--- 01_var_let_const.js
+++ 01_var_let_const.js
@@ -2,6 +2,7 @@ var empRollNo = "1234"
 let empName = "name"
 const empId = 12
 empSalary = 12000
+let rr;
 
 
 // we can change const variable value
@@ -16,9 +17,9 @@ empSalary = 30000
 
 console.log('empId, empRollNo,empName,empSalary',empId, empRollNo, empName, empSalary)
 
-console.table([empId, empRollNo,empName,empSalary])
+console.table([empId, empRollNo,empName,empSalary, rr])
 
 /*
-  prefer to use let and const , 
-  do not use var as its have no scope , its global,
+  prefer to use let and const, 
+  do not use var as its have no scope , its global scope and impact on block/function scope sometime
 */
---
+++ 02_data_type.js
@@ -0,0 +1,36 @@
+'use strict'; // trea all JS code in newer version like classes, module, arrow function, hooks, ...etc
+
+// alert('ssss'); // it will throw error due to we used node js , not browser.
+
+
+console.log(4 
+    + 3
+) // no use this type of code , 
+
+
+console.log(3 + 3)  // make code proper structure and readability 
+
+console.log("Navin")
+
+
+// data types
+
+let name = "Navin"
+let age = 28
+let isLoggedIn =  true
+
+
+// number => ( range 2 - to power 53 )
+// bigInt = used to big number like stock market , bit number site
+
+// string => ""
+// boolean => true | false
+// null => it is standalone value, null it type
+// undefined => when value not define of variable
+// symbol => to find uniqueness, its unique
+
+
+console.log(typeof undefined) // type undefined
+console.log(typeof null) // type object
+
+
