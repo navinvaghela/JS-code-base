34eb918b546a27fa8c5ebe68352a705b5707172d
navinvaghela <navin7771338@gmail.com>
2025-07-08 14:38:20 +0530

    var-let-const understanding

---
+++ 01_var_let_const.js
@@ -0,0 +1,24 @@
+var empRollNo = "1234"
+let empName = "name"
+const empId = 12
+empSalary = 12000
+
+
+// we can change const variable value
+// empId = 3333 // immutable
+
+
+//we can change below variables
+empRollNo = 2323
+empName = "www"
+empSalary = 30000
+
+
+console.log('empId, empRollNo,empName,empSalary',empId, empRollNo, empName, empSalary)
+
+console.table([empId, empRollNo,empName,empSalary])
+
+/*
+  prefer to use let and const , 
+  do not use var as its have no scope , its global,
+*/
